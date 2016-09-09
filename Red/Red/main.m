7646e3800294179b08db785bca6e6eb5e890a1f1
DerrickQin <admin@admintekiMacBook-Pro.local>
2016-09-09 18:39:02 +0800

    Init Project 初始化项目

---
+++ Red/Red/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Red
+//
+//  Created by admin on 16/9/9.
+//  Copyright © 2016年 Derrick_Qin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
